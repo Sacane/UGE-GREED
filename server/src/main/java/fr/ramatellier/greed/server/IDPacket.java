837f7a2308ec04d0fa3ee0a7bec44e19d3f036dd
qtdrake <quentintellier01@yahoo.com>
2023-03-10 10:52:32 +0100

    Add informations to send connection packet

--- server/src/main/java/fr/ramatellier/greed/server/IDPacket.java
+++ server/src/main/java/fr/ramatellier/greed/server/IDPacket.java
@@ -1,14 +1,29 @@
 package fr.ramatellier.greed.server;
 
+import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 
 public class IDPacket implements Packet {
     private final IPPacket ipPacket;
     private final int port;
+    private final InetSocketAddress address;
 
-    public IDPacket(String address, int port) {
-        ipPacket = new IPPacket(address);
-        this.port = port;
+    public IDPacket(InetSocketAddress address) {
+        ipPacket = new IPPacket(address.getHostName());
+        this.port = address.getPort();
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return ipPacket.getAddress();
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public InetSocketAddress getSocket() {
+        return address;
     }
 
     public String getAddress() {
